8413c9a6fc8d571e8107a8238038bf268c6e3c14
Roj2021 <botani2019@gmail.com>
2023-03-29 15:48:36 -0400

    The homework git05

---
+++ src/Task1.java
@@ -0,0 +1,16 @@
+public class Task1 {
+    public static void main(String[] args) {
+        
+    
+    /*
+    /Step2
+    Clone the pushed repo as being a member of a project.
+    Update the code and make it print integers till 100.
+    Push the code to the same repo/
+   
+     */
+        for (int i = 0; i < ; i++) {
+            
+        }
+    } 
+}
